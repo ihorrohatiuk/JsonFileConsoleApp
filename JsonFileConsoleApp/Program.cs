005012b1acd7b227ab088de22d19fd1db7edfa6d
IhorRohatiuk <rogatyuk60605@gmail.com>
2024-08-02 10:27:21 +0200

    Add updating Json file.

--- JsonFileConsoleApp/Program.cs
+++ JsonFileConsoleApp/Program.cs
@@ -1,24 +1,40 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using JsonFileConsoleApp.Models;
 using JsonFileConsoleApp.Services;
+using Newtonsoft.Json;
 
 namespace JsonFileConsoleApp;
 
 public class Program
 {
+    private static readonly string PathToJsonFile = "../../../employees.json";
+
     static void Main(string[] args)
     {
+        // Reading info from Json
         var company = GetCompanyFromJsonAsync().Result;
 
+        // Displaying in console
         DisplayService.DisplayCompanyInfo(company);
         DisplayService.DisplayEmployees(company.Employees);
+
+        // Changing some info
+        company.Name = "Software Inc.";
+
+        // Updating Json
+        UpdateJsonFile(company);
+
+        // Displaying new info in console
+        DisplayService.DisplayCompanyInfo(company);
+
     }
 
     static async Task<Company> GetCompanyFromJsonAsync()
     {
         Company company;
-        var parseService = new ParseService("../../../employees.json");
+        var parseService = new ParseService(PathToJsonFile);
 
         try
         {
@@ -31,5 +47,11 @@ public class Program
         }
 
         return company;
+    }
+
+    static void UpdateJsonFile(Company company)
+    {
+        var content = JsonConvert.SerializeObject(company, Formatting.Indented);
+        File.WriteAllText(PathToJsonFile, content);
     }
 }
